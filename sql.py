6eb646aada0c3159c9db625269075268c80b06e3
sif-gondy <72805403+sif-gondy@users.noreply.github.com>
2024-11-27 10:06:03 +0100

    Update sql.py

--- sql.py
+++ sql.py
@@ -2,7 +2,9 @@ COMMAND = """
 SELECT
     solutionrewrites.short_name as 'solution_name',
     solutionrewrites.one_sentence_description as 'solution_description',
-   CONCAT('https://solarimpulse.com/solutions-explorer/', solutionrewrites.slug) as solution_explorer,
+    CONCAT('https://solarimpulse.com/solutions-explorer/', solutionrewrites.slug) as solution_explorer,
+    "Labelled" as "solution_status" ,
+    solutions.solution_status_date as 'solution_date',
     companies.address,
     companies.geoLatitude,
     companies.geoLongitude,
@@ -10,8 +12,7 @@ SELECT
     companies.city,
     companies.name AS 'company_name',
     companies.about_us as 'about_company',
-    companies.description as 'company_description',
-    companies.domain as 'website'
+    companies.description as 'company_description'
 FROM
     companies
 left outer join solutions on companies.id = solutions.company_id
@@ -21,7 +22,12 @@ UNION
 SELECT 
     solutionrewrites.short_name as 'solution_name',
     solutionrewrites.one_sentence_description as 'solution_description',
-   CONCAT('https://solarimpulse.com/solutions-explorer/', solutionrewrites.slug) as solution_explorer,
+    CONCAT('https://solarimpulse.com/solutions-explorer/', solutionrewrites.slug) as solution_explorer,
+        CASE 
+        WHEN is_labeled = 1 THEN 'Labelled'
+        WHEN is_labeled IS NULL AND is_listed = 1 THEN 'Listed'
+    END AS 'solution_status',
+    ssfs.ssf_status_date as 'solution_date',
     companies.address,
     companies.geoLatitude,
     companies.geoLongitude,
@@ -29,11 +35,10 @@ SELECT
     companies.city,
     companies.name AS 'company_name',
     companies.about_us as 'about_company',
-    companies.description as 'company_description',
-    companies.domain as 'website'
+    companies.description as 'company_description'
 FROM
     companies
 left outer join ssfs on companies.id = ssfs.company_id
 left outer join solutionrewrites on ssfs.id = solutionrewrites.ssf_id
-WHERE companies.country = "France" and ssfs.ssf_status_id = 12
+WHERE companies.country = "France" and (ssfs.is_labeled = 1 OR ssfs.is_listed = 1)
 """
